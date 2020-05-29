52935bf09f8140eb7bc5faf2cc9ea15472b96d3e
jhjshin <hyunjoo940926@gmail.com>
2020-05-28 21:52:21 -0400

    created data in backend server.js and connected with front-end Main.js

---
+++ sprint-3/Client/src/components/Header/Header.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './Header.scss'
+import logoImage from '../../assets/Logo/Logo-brainflix.svg';
+import searchIcon from '../../assets/Icons/SVG/Icon-search.svg'
+import uploadIcon from '../../assets/Icons/SVG/Icon-upload.svg';
+import userImage from '../../assets/Images/Mohan-muruge.jpg';
+
+import {Link} from 'react-router-dom';
+
+
+// HEADER
+
+const Header = () => {
+ 
+    return (
+        <header className="header">
+
+            <div className="header--inner">
+                <Link to="/" className="header__logo">
+                    <img  src={logoImage} alt="brainFlix logo"/>
+                </Link>
+                <div className="header--flexbox">
+                    <form className="header__search-container">
+                        <img className="header__search-icon" src={searchIcon} alt="searchIcon"/>
+                        <input className="header__search" type="text" placeholder="Search"/>
+                    </form>
+                    <div className="header__btn-container">
+                        <Link to="/upload" className="header__upload">
+                            <button className="btn">
+                                <img className="header__upload-icon" src={uploadIcon} alt="uploadIcon"/>
+                            <p className="header__upload-text">UPLOAD</p>
+                            </button>
+                        </Link>
+                        <img className="header__userImg" src={userImage} alt="userImage"/>
+                    </div>
+                </div>
+                
+
+            </div>
+            
+        </header>
+    )
+
+}
+
+export default Header;
---
+++ sprint-3/Server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const videoRoutes = require('./routes/videoRoutes');
+
+const port = process.env.PORT || 8080;
+
+app.use((req, res, next) => {
+    console.log('Request: ', req.path, Date.now());
+    next();
+})
+
+app.use(express.json());
+
+app.use(express.static('assets'));
+
+app.use(cors());
+
+app.use('/', videoRoutes);
+
+app.listen(port, () => console.log(`Server started at ${port}`))
